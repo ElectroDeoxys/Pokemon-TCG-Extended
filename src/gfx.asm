3043fd14101c25e9d86d2d7c12a84ceb886b7db0
Electro <ElectroDeoxys@gmail.com>
2026-01-06 17:18:43 +0000

    Make DuelCgbSymbolGraphics continguous

--- src/gfx.asm
+++ src/gfx.asm
@@ -26,11 +26,9 @@ DuelCardHeaderGraphics::
 INCBIN "gfx/duel/card_headers.2bpp"
 
 DuelCgbSymbolGraphics::
-INCBIN "gfx/duel/cgb_symbols.2bpp", $0, $808
+INCBIN "gfx/duel/cgb_symbols.2bpp"
 
 SECTION "Gfx 2", ROMX
-
-INCBIN "gfx/duel/cgb_symbols.2bpp", $808, $8
 
 DuelOtherGraphics::
 INCBIN "gfx/duel/other.2bpp"
